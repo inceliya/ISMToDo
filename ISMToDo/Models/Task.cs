74d17f36f6d1bdcb4a72215243b529fa2ea045db
Maksim Zakharchuk <zahar2002263802@gmail.com>
2020-06-10 11:47:31 +0300

    start

---
+++ ISMToDo/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ISMToDo.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+    }
+}
